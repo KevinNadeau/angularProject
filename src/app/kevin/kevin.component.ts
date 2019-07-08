32e513902d27d1dd99fb133b5e769265767439c8
KevinNadeau <kevin.nadeau@hotmail.fr>
2019-07-08 02:19:50 +0200

    1er commit

---
+++ src/app/kevin/kevin.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { logging } from 'protractor';
+
+@Component({
+  selector: 'app-kevin',
+  templateUrl: './kevin.component.html',
+  styleUrls: ['./kevin.component.scss']
+})
+export class KevinComponent implements OnInit {
+  addition : number = 15;
+  y: number;
+  x: number;
+  isLogedIn : boolean = false;
+  monNom : string;
+  constructor() {
+    this.x = 7;
+    this.y = 3;
+
+  }
+
+  ngOnInit() {
+}
+    multi(){
+      return this.x * this.y;
+    }
+    login(){
+      setTimeout(() => {
+        this.isLogedIn = true
+      alert('Vous etes maintenant connecté en tant que' + this.monNom );
+    }, 4000);
+
+  }
+}
+
